89698ff2123e09b57c94f989a1bbf7201f6f2fd8
dchowdhury-mdsol <daiyanch22@gmail.com>
2023-09-21 18:49:44 -0400

    updating git ignore and adding fixes for dynamoDB

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "${var.product}-${var.environment}"
-    key    = "terraform"
-    region = var.region
+
   }
 }
 
@@ -19,19 +17,36 @@ provider "aws" {
 }
 
 locals {
-  naming = "${var.product}-${var.environment}"
+  name_convention = "${var.product}-${var.environment}"
 }
 
 data "aws_caller_identity" "current" {}
 
 resource "aws_dynamodb_table" "dynamodb" {
-  name           = "${locals.naming}"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 10
-  write_capacity = 10
-  hash_key       = "QueryID"
-  range_key      = "QueryType"
-  stream_enabled = true
+  name             = "${local.name_convention}"
+  billing_mode     = "PROVISIONED"
+  read_capacity    = 10
+  write_capacity   = 10
+  hash_key         = "QueryID"
+  range_key        = "QueryType"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+  
+  global_secondary_index {
+    name            = "QueryIndex"
+    hash_key        = "Query"
+    projection_type = "ALL"  # You can choose the appropriate projection type
+    read_capacity   = 10
+    write_capacity  = 10
+  }
+
+  global_secondary_index {
+    name            = "QueryResponseIndex"
+    hash_key        = "QueryResponse"
+    projection_type = "ALL"  # You can choose the appropriate projection type
+    read_capacity   = 10
+    write_capacity  = 10
+  }
 
   attribute {
     name = "QueryID"
