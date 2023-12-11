21e04546a0aac93da1a0146a28d710f7eb137c83
dchowdhury-mdsol <daiyanch22@gmail.com>
2023-12-11 16:41:24 -0500

    adding text code

--- src/bash/table-population.sh
+++ src/bash/table-population.sh
@@ -2,17 +2,24 @@
 set -e
 
 # Make sure AWS CLI is installed and configured with the necessary permissions
-: ${ENVIRONMENT:?Set to working environment}
+
+# Assigning command line arguments to variables
+ENVIRONMENT=$1
+ID=$2
+QUERY=$3
+QUERY_TYPE=$4
+COMPLETION=$5
+RESPONSE_ACCEPTED=${6:-false} # Default to false if not provided
+ACCEPTANCE_TARGET=${7:-0.97} # Default to 0.97 if not provided
+ACTUAL_RESPONSE=${8:-None} # Default to None if not provided
+
 TABLE_NAME="legalese-ease-$ENVIRONMENT"
 
-# Setting all other required env vars
+# Checking if required variables are set
+: ${ENVIRONMENT:?Set to working environment}
 : ${ID:?Set to ID}
 : ${QUERY:?Set to AI Query}
 : ${QUERY_TYPE:?Set to Query Type}
 : ${COMPLETION:?Set to AI Response}
-: ${RESPONSE_ACCEPTED:=false} # Defaults response as accepted as false, will be true if reviewed by lawyer
-: ${ACCEPTANCE_TARGET:=0.97} # Default current GPT 3.5 acceptance targets
-: ${ACTUAL_RESPONSE:=None} # Default will set to None for actual response and assume query response is accepted
-
 
 python src/python/QueryFormat.py "$ID" "$QUERY" "$COMPLETION" "$RESPONSE_ACCEPTED" "$ACCEPTANCE_TARGET" "$QUERY_TYPE" "$ACTUAL_RESPONSE" "$ENVIRONMENT"
